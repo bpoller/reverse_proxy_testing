b90312c212b44bbb1738b3181d22576305dbc6c9
bert <bpoller@ekito.fr>
2016-01-28 12:11:20 +0100

    Added logging support.

--- src/test/java/org/bpoller/proxytest/ContainerTest.java
+++ src/test/java/org/bpoller/proxytest/ContainerTest.java
@@ -1,13 +1,20 @@
 package org.bpoller.proxytest;
 
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import static org.junit.Assert.assertTrue;
 
 public class ContainerTest {
 
+    private static final Logger logger = LoggerFactory.getLogger(ContainerTest.class);
+
+
     @Test
     public void shouldWork() {
+
+        logger.info("I am logging !");
         assertTrue("This should not fail", true);
     }
 
