e5d363dca3011e0a7fb5ca600c7aed31ea712a59
bert <bpoller@ekito.fr>
2016-01-28 15:52:58 +0100

    Added functionality to pull an image and create a container.

--- src/test/java/org/bpoller/proxytest/ContainerTest.java
+++ src/test/java/org/bpoller/proxytest/ContainerTest.java
@@ -1,21 +1,59 @@
 package org.bpoller.proxytest;
 
+import com.github.dockerjava.api.DockerClient;
+import com.github.dockerjava.api.command.CreateContainerResponse;
+import com.github.dockerjava.api.model.Image;
+import com.github.dockerjava.core.DockerClientBuilder;
+import com.github.dockerjava.core.command.PullImageResultCallback;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.List;
+
+import static java.util.Arrays.asList;
 import static org.junit.Assert.assertTrue;
 
 public class ContainerTest {
 
     private static final Logger logger = LoggerFactory.getLogger(ContainerTest.class);
 
+    private static DockerClient docker;
+
+    @BeforeClass
+    public static void init() {
+
+        docker = DockerClientBuilder.getInstance("unix:///var/run/docker.sock").build();
+    }
 
     @Test
-    public void shouldWork() {
+    public void shouldStartHelloWorldContainer() {
 
-        logger.info("I am logging !");
+        pullImageIfNotExists("busybox:latest");
+
+        CreateContainerResponse container = docker.createContainerCmd("busybox:latest")
+                .withCmd("touch")
+                .exec();
+
+        logger.info("container with id {} created ", container.getId());
+
+
         assertTrue("This should not fail", true);
     }
 
+    private boolean existsLocally(String searchString) {
+        List<Image> dockerSearch = docker.listImagesCmd().exec();
+        return dockerSearch.stream().filter(image -> asList(image.getRepoTags()).stream()
+                .filter(tag -> tag.contains(searchString)).count() > 0)
+                .count() > 0;
+    }
+
+    private void pullImageIfNotExists(String imageName) {
+        if (!existsLocally(imageName)) {
+            PullImageResultCallback imagePulled = new PullImageResultCallback();
+            docker.pullImageCmd(imageName).exec(imagePulled);
+            imagePulled.awaitSuccess();
+        }
+    }
 }
