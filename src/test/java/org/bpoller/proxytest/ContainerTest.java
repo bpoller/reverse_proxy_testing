35667494a05693e83655e11ec6770f8d157ffab2
bert <bpoller@ekito.fr>
2016-01-28 11:59:27 +0100

    Wrote first test.

---
+++ src/test/java/org/bpoller/proxytest/ContainerTest.java
@@ -0,0 +1,14 @@
+package org.bpoller.proxytest;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class ContainerTest {
+
+    @Test
+    public void shouldWork() {
+        assertTrue("This should not fail", true);
+    }
+
+}
