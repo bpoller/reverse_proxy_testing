39273b148b9e44cebbccc9822fe9d412a52d57e4
bert <bpoller@ekito.fr>
2016-02-08 11:35:52 +0100

    Extracted reusable utility methods into DockerTest.

--- src/test/java/org/bpoller/haproxy/DockerTest.java
+++ src/test/java/org/bpoller/haproxy/DockerTest.java
@@ -27,6 +27,40 @@ public abstract class DockerTest {
         docker = DockerClientBuilder.getInstance(config).build();
     }
 
+    protected void createNetwork(String networkName) {
+        docker.createNetworkCmd().withName(networkName).withDriver("bridge").exec();
+    }
+
+    protected void removeNetwork(String networkName) {
+        docker.listNetworksCmd().exec().
+                stream()
+                .filter(network -> network.getName().equals(networkName))
+                .forEach(network -> docker.removeNetworkCmd(network.getId()).exec());
+    }
+
+    protected void addContainerToNetwork(String containerName, String networkName) {
+        docker.connectToNetworkCmd().withContainerId(containerName).withNetworkId(networkName).exec();
+    }
+
+    protected void stopAndRemoveContainer(String containerName) {
+        docker.listContainersCmd().withShowAll(true).exec()
+                .stream()
+                .filter(container -> asList(container.getNames()).contains("/" + containerName))
+                .forEach(container -> {
+
+                    if (!container.getStatus().contains("Exit")) {
+                        docker.stopContainerCmd(containerName).exec();
+                    }
+                    docker.removeContainerCmd(containerName).exec();
+                });
+    }
+
+    protected String createAndStartContainer(String image, String containerName) {
+        String id = docker.createContainerCmd(image).withName(containerName).exec().getId();
+        docker.startContainerCmd(containerName).exec();
+        return id;
+    }
+
     protected boolean existsLocally(String searchString) {
         List<Image> dockerSearch = docker.listImagesCmd().exec();
         return dockerSearch.stream().filter(image -> asList(image.getRepoTags()).contains(searchString))
--- src/test/java/org/bpoller/haproxy/HaProxyConfTest.java
+++ src/test/java/org/bpoller/haproxy/HaProxyConfTest.java
@@ -2,10 +2,10 @@ package org.bpoller.haproxy;
 
 
 import com.github.dockerjava.api.model.Network;
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
-import static java.util.Arrays.asList;
 import static org.junit.Assert.assertTrue;
 
 public class HaProxyConfTest extends DockerTest {
@@ -17,34 +17,26 @@ public class HaProxyConfTest extends DockerTest {
 
     @Before
     public void setup() {
+        stopAndRemoveContainer(containerName);
+        removeNetwork(networkName);
+        createNetwork(networkName);
 
-        docker.listContainersCmd().exec()
-                .stream()
-                .filter(container -> asList(container.getNames()).contains("/" + containerName))
-                .forEach(container -> {
-                    docker.stopContainerCmd(containerName).exec();
-                    docker.removeContainerCmd(containerName).exec();
-                });
-
-        docker.listNetworksCmd().exec().
-                stream()
-                .filter(network -> network.getName().equals(networkName))
-                .forEach(network -> docker.removeNetworkCmd(network.getId()).exec());
-
-
-        docker.createNetworkCmd().withName(networkName).withDriver("bridge").exec();
         pullImageIfNotExists("mongo:latest");
-        containerId = docker.createContainerCmd("mongo:latest").withName(containerName).exec().getId();
-
-        docker.startContainerCmd(containerName).exec();
+        containerId = createAndStartContainer("mongo:latest", containerName);
     }
-
 
     @Test
     public void shouldConnectContainerToNetwork() {
-        docker.connectToNetworkCmd().withContainerId(containerName).withNetworkId(networkName).exec();
-        Network network = docker.inspectNetworkCmd().withNetworkId(networkName).exec();
+        addContainerToNetwork(containerName, networkName);
 
+        Network network = docker.inspectNetworkCmd().withNetworkId(networkName).exec();
         assertTrue("The container should be connected to the network but it doesn't seem so.", network.getContainers().keySet().contains(containerId));
+    }
+
+    @After
+    public void tearDown() {
+        stopAndRemoveContainer(containerName);
+        removeNetwork(networkName);
+
     }
 }
